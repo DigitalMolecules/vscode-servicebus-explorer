58f23c4e76250f10fcbb62115ac2520314e2874f
juancarlos <andreujuanc@hotmail.com>
2019-09-03 19:33:05 +0200

    context values

--- src/serviceBusProvider.ts
+++ src/serviceBusProvider.ts
@@ -3,10 +3,10 @@ import * as fs from 'fs';
 import * as path from 'path';
 
 const NAMESPACE_CONNECTIONS  = 'dm.sbe.connections';
-export class ServiceBusProvider implements vscode.TreeDataProvider<NameSpace> {
+export class ServiceBusProvider implements vscode.TreeDataProvider<ExplorerItemBase> {
 	
-	private _onDidChangeTreeData: vscode.EventEmitter<NameSpace | undefined> = new vscode.EventEmitter<NameSpace | undefined>();
-	readonly onDidChangeTreeData: vscode.Event<NameSpace | undefined> = this._onDidChangeTreeData.event;
+	private _onDidChangeTreeData: vscode.EventEmitter<ExplorerItemBase | undefined> = new vscode.EventEmitter<ExplorerItemBase | undefined>();
+	readonly onDidChangeTreeData: vscode.Event<ExplorerItemBase | undefined> = this._onDidChangeTreeData.event;
 
 	state: vscode.Memento;
 
@@ -14,24 +14,37 @@ export class ServiceBusProvider implements vscode.TreeDataProvider<NameSpace> {
 		this.state = context.workspaceState;
 	}
 	
-	getTreeItem(element: NameSpace): vscode.TreeItem {
+	getTreeItem(element: ExplorerItemBase): vscode.TreeItem {
 		return element;
 	}
 
-    getChildren(element?: NameSpace): Thenable<NameSpace[]> {
-		if(!element){
+    getChildren(element?: ExplorerItemBase): Thenable<ExplorerItemBase[]> {
+		
+		//On undefined we get the namespaces
+		if(!element){ 
 			var connections = this.state.get<NameSpaceData[]>(NAMESPACE_CONNECTIONS, []);
 			return Promise.resolve(
 				[
-					... connections.map(c=> new NameSpace(c.name, "1", vscode.TreeItemCollapsibleState.Collapsed))
+					... connections.map(c=> new NameSpace(c.name, vscode.TreeItemCollapsibleState.Expanded))
 				]
 			);
+		}
+		else if(element instanceof NameSpace){
+			return Promise.resolve([
+				new QueueList('Queues', vscode.TreeItemCollapsibleState.Collapsed),
+				new TopicList('Topics', vscode.TreeItemCollapsibleState.Collapsed),
+			]);
 		}
 		return Promise.resolve([]);
 	}
 
 	refresh(): void {
-		
+		var items = this.state.get<NameSpaceData[]>(NAMESPACE_CONNECTIONS, []);
+		items.forEach(element => {
+			///TODO Refresh
+		});
+		this.state.update(NAMESPACE_CONNECTIONS, items );
+		this._onDidChangeTreeData.fire();
 	}
 
 	addNamespace(item: NameSpaceData){
@@ -47,11 +60,13 @@ interface NameSpaceData{
 	connection: string;
 }
 
-export class NameSpace extends vscode.TreeItem {
+
+
+
+export class ExplorerItemBase extends vscode.TreeItem {
 
 	constructor(
 		public readonly label: string,
-		private version: string,
 		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
 		public readonly command?: vscode.Command
 	) {
@@ -59,11 +74,11 @@ export class NameSpace extends vscode.TreeItem {
 	}
 
 	get tooltip(): string {
-		return `${this.label}-${this.version}`;
+		return `${this.label}`;
 	}
 
 	get description(): string {
-		return this.version;
+		return 'description';
 	}
 
 	iconPath = {
@@ -71,6 +86,87 @@ export class NameSpace extends vscode.TreeItem {
 		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
 	};
 
-	contextValue = 'dependency';
+	contextValue = 'base';
+
+}
+
+export class NameSpace extends ExplorerItemBase {
+
+	constructor(
+		label: string,
+		collapsibleState: vscode.TreeItemCollapsibleState,
+		command?: vscode.Command
+	) {
+		super(label, collapsibleState, command);
+	}
+
+	get tooltip(): string {
+		return `${this.label}`;
+	}
+
+	get description(): string {
+		return '(0)';
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'namespace';
+
+}
+
+export class TopicList extends ExplorerItemBase {
+
+	constructor(
+		label: string,
+		collapsibleState: vscode.TreeItemCollapsibleState,
+		command?: vscode.Command
+	) {
+		super(label, collapsibleState, command);
+	}
+
+	get tooltip(): string {
+		return `${this.label}`;
+	}
+
+	get description(): string {
+		return '(0)';
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'topiclist';
+
+}
+
+export class QueueList extends ExplorerItemBase {
+
+	constructor(
+		label: string,
+		collapsibleState: vscode.TreeItemCollapsibleState,
+		command?: vscode.Command
+	) {
+		super(label, collapsibleState, command);
+	}
+
+	get tooltip(): string {
+		return `${this.label}`;
+	}
+
+	get description(): string {
+		return '(0)';
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'queuelist';
 
 }
