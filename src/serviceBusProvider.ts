9901568fda8b163ac4559fde9b40b7f3988f2e1a
konradmainzer@outlook.com <konradmainzer@outlook.com>
2019-09-03 09:19:45 +0200

    Post re index

---
+++ src/serviceBusProvider.ts
@@ -0,0 +1,48 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+
+export class ServiceBusProvider implements vscode.TreeDataProvider<NameSpace> {
+
+	onDidChangeTreeData?: vscode.Event<any>;    
+	
+	getTreeItem(element: NameSpace): vscode.TreeItem {
+		return element;
+	}
+
+    getChildren(element?: NameSpace): Thenable<NameSpace[]> {
+		return Promise.resolve(
+			[
+				new NameSpace("Label1", "1", vscode.TreeItemCollapsibleState.Collapsed)
+			]
+		)
+	}
+}
+
+export class NameSpace extends vscode.TreeItem {
+
+	constructor(
+		public readonly label: string,
+		private version: string,
+		public readonly collapsibleState: vscode.TreeItemCollapsibleState,
+		public readonly command?: vscode.Command
+	) {
+		super(label, collapsibleState);
+	}
+
+	get tooltip(): string {
+		return `${this.label}-${this.version}`;
+	}
+
+	get description(): string {
+		return this.version;
+	}
+
+	iconPath = {
+		light: path.join(__filename, '..', '..', 'resources', 'light', 'dependency.svg'),
+		dark: path.join(__filename, '..', '..', 'resources', 'dark', 'dependency.svg')
+	};
+
+	contextValue = 'dependency';
+
+}
