d293f27507fafaf0f23f874829b2014af56f4125
Konrad Mainzer <konradmainzer@outlook.com>
2019-09-05 15:52:55 +0200

    removed number from desc

--- src/common/explorerItemBase.ts
+++ src/common/explorerItemBase.ts
@@ -23,7 +23,7 @@ export class ExplorerItemBase extends vscode.TreeItem {
 	}
 	
 	public get description(): string {
-		return this.itemData.error ? 'ERROR' : '(0)';
+		return this.itemData.error ? 'ERROR' : '';
 	}
 
 	contextValue = 'base';
