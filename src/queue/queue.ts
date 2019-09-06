eb445fadb8e115d3e6230f5e384b470444c15ba7
Norbert Spiteri <nspiteri@kaya-consulting.com>
2019-09-06 14:36:30 +0200

    fix correct inheritance of queue

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -2,14 +2,14 @@ import { TreeItemCollapsibleState, Command } from "vscode";
 import { ExplorerItemBase, IItemData } from "../common/explorerItemBase";
 import { QueueList } from "./queueList";
 
-export class Queue extends QueueList {
+export class Queue extends ExplorerItemBase {
 
 	constructor(
 		public itemData: IItemData,
 		title: string,
 		command?: Command
 	) {
-		super(itemData, 0, 0, command);
+		super(itemData, 0, command);
 		this.label = title;
 	}
 
