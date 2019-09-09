7d3ad293beae1cbdf7a5eb7d9b3d8352aa79d56a
juancarlos <andreujuanc@hotmail.com>
2019-09-09 11:01:24 +0200

    message template

--- src/messages/messageWebView.ts
+++ src/messages/messageWebView.ts
@@ -3,10 +3,67 @@ import { Subscription } from '../topic/subscription';
 
 export class MessageWebView {
 
-    open(context: vscode.ExtensionContext, node: Subscription) {
+    panel: vscode.WebviewPanel | undefined;
 
+    async getMessages(): Promise<any[]> {
+        return [
+            1,
+            2
+        ];
+    }
 
-        const panel = vscode.window.createWebviewPanel(
+    async renderMessages(title: string, messages: any[] ): Promise<void> {
+        if(!this.panel){
+            return;
+        }
+
+        const messageTable: string = 
+        
+            messages.map(x=>
+                `
+                    <tr>
+                        <td>
+                            Message 1
+                        </td>
+                        <td>
+                            <button onclick="showMessage()">Get Messages</button>
+                        </td>
+                    </tr>
+                `
+            )
+            .reduce( (p, c)=> p+=c , '');
+
+        this.panel.webview.html = `
+		<!DOCTYPE html>
+		<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0">
+			<title>Cat Coding</title>
+		</head>
+		<body>
+                <h1>Messages (${title})</h1>
+                <script >
+                    const vscode = acquireVsCodeApi();
+                    function showMessage(){
+                        vscode.postMessage({
+                            command: 'serviceBusExplorer.showMessage',
+                            text: 'Potatoes'
+                        })
+                    }
+                </script>
+                <table>
+                    ${messageTable}
+                </table>
+			</body>
+			
+			`;
+
+    }
+
+    async open(context: vscode.ExtensionContext, node: Subscription) : Promise<void> {
+
+        this.panel = vscode.window.createWebviewPanel(
             'messagelist', // Identifies the type of the webview. Used internally
             `Messages (${node.label})`, // Title of the panel displayed to the user
             vscode.ViewColumn.One, // Editor column to show the new webview panel in.
@@ -15,7 +72,7 @@ export class MessageWebView {
             }
         );
 
-        panel.webview.onDidReceiveMessage(
+        this.panel.webview.onDidReceiveMessage(
             message => {
                 switch (message.command) {
                     case 'serviceBusExplorer.showMessage':
@@ -27,40 +84,11 @@ export class MessageWebView {
             context.subscriptions
         );
 
-        panel.webview.html = `
-		<!DOCTYPE html>
-		<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Cat Coding</title>
-		</head>
-		<body>
-						<h1>Messages (${node.label})</h1>
-						<script >
-							const vscode = acquireVsCodeApi();
-							function showMessage(){
-								vscode.postMessage({
-									command: 'serviceBusExplorer.showMessage',
-									text: 'Potatoes'
-								})
-							}
-						</script>
-						<table>
-							<tr>
-								<td>
-									Message 1
-								</td>
-								<td>
-									<button onclick="showMessage()">Get Messages</button>
-								</td>
-							</tr>
-						</table>
-			</body>
-			
-			`;
+        const messages = await this.getMessages();
 
-        panel.onDidDispose(() => {
+        await this.renderMessages(node.label, messages);
+
+        this.panel.onDidDispose(() => {
 
         }, null, context.subscriptions);
     }
