9bb7ae51ae9b8281852363f766a0793fba5867d0
Konrad Mainzer <konradmainzer@outlook.com>
2019-09-09 11:53:14 +0200

    Added placeholder for deadletter queue count on queue

--- src/queue/queue.ts
+++ src/queue/queue.ts
@@ -8,10 +8,16 @@ export class Queue extends ExplorerItemBase {
 		public itemData: IItemData,
 		title: string,
 		collapsibleState: TreeItemCollapsibleState = TreeItemCollapsibleState.None,
+		public messageCount: number = 0,
+		public deadLettetCount: number = 0,
 		command?: Command
 	) {
 		super(itemData, collapsibleState, command);
 		this.label = title;
+	}
+
+	public get description(): string {
+		return `(${this.messageCount.toLocaleString()}) (${this.deadLettetCount.toLocaleString()})`;
 	}
 
 	contextValue = 'queue';
