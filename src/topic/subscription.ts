7e9e099e803f37ff1d17696e8783bdf2a53f7f4f
Konrad Mainzer <konradmainzer@outlook.com>
2019-10-11 16:59:54 +0200

    Retreiving message count detail to be able to display active and deadletter meassge counts

--- src/topic/subscription.ts
+++ src/topic/subscription.ts
@@ -1,8 +1,9 @@
 import { ExplorerItemBase, IItemData } from "../common/explorerItemBase";
-import { TreeItemCollapsibleState, Command, ExtensionContext } from "vscode";
+import { TreeItemCollapsibleState, Command, ExtensionContext, Uri } from "vscode";
 import { ISubscription } from "../client/models/ISubscriptionDetails";
 import { MessageWebView } from "../messages/messageWebView";
 import path from 'path';
+import { URL } from "url";
 
 export class Subscription extends ExplorerItemBase {
 
@@ -26,10 +27,12 @@ export class Subscription extends ExplorerItemBase {
 	) {
 		super(itemData, collapsibleState, command);
 		this.label = subscription.title;
-		this.messageCount = subscription.content.SubscriptionDescription.MessageCount;
+		this.messageCount = subscription.content.SubscriptionDescription.CountDetails.ActiveMessageCount;
+		this.deadLettetCount = subscription.content.SubscriptionDescription.CountDetails.DeadLetterMessageCount;
 	}
 
 	public get description(): string {
+
 		return `(${this.messageCount.toLocaleString()}) (${this.deadLettetCount.toLocaleString()})`;
 	}
 
