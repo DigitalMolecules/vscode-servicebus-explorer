4353e6a0c0ecc42b169201801ef877e589376bcc
Sharmila Sonavane <S.Sonavane@terberg.com>
2019-10-17 13:41:19 +0200

    added compile task b4 running test

--- src/test/suite/providers/MessageProvider.test.ts
+++ src/test/suite/providers/MessageProvider.test.ts
@@ -9,7 +9,7 @@ import { MessageStoreInstance } from '../../../common/global';
 suite('MessageProvider Test Suite', () => {
 	const body = { root: { "node": "data" } };
 	const messageid="TestMessageId";
-	
+	var messageProvider=new MessageProvider();
 	before(() => {
 		vscode.window.showInformationMessage('Start all tests.');
 		var testmsg: any={};
@@ -17,8 +17,8 @@ suite('MessageProvider Test Suite', () => {
 		MessageStoreInstance.setMessage("TestMessageId",testmsg);
 	});
 
-	test('Test Provide Text Document Content', () => {
-		let messageProvider=new MessageProvider();
+	test('Test provide text document content', () => {
+		
 		let testUri = vscode.Uri.parse(`http://www.msft.com/some/path?query&messageid=TestMessageId`);
 
 		let result= messageProvider.provideTextDocumentContent(testUri);
@@ -26,5 +26,13 @@ suite('MessageProvider Test Suite', () => {
 		let resultObject = JSON.parse(result);
 
 		assert.equal(resultObject.root.node ,body.root.node);
+	 });
+	 
+	 test('Test  Provide empty text document content if missing message id', () => {
+		let testUri = vscode.Uri.parse(`http://www.msft.com/some/path?query&messageid`);
+
+		let result= messageProvider.provideTextDocumentContent(testUri);
+
+		assert.equal(result,"");
  	});
 });
